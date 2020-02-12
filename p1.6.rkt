ea16e879914b8b4172e8bc554be5a3f7d8d921de
lyf0601 <lyfmssjlqw@163.com>
2020-02-12 21:45:43 +0800

    Update p1.6.rkt

--- p1.6.rkt
+++ p1.6.rkt
@@ -26,3 +26,11 @@ if (good-enough? guess x)
 (sqrt 9)
 (sqrt (+ 100 37))
 
+*|
+代码如上，如果使用 new-if 调用函数会内存溢出。
+
+尝试1：用 if 重新写 new-if-mine，调用函数结果还是内存溢出。
+尝试2：减少 good-enough? 函数的精度，0.001改为0.1，调用函数结果还是内存溢出。
+
+猜测：因为之前书中也说 schema 的编译方式采用的是应用序，所以像 improve average square abs good-enough？ sqrt 都可以直接算出结果的函数，是可以放在递归函数中的，而像 new-if 这种不能直接算出结果的函数，调用了就会内存溢出。
+|*
